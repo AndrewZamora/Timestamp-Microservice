1aa09f239b2479e222265b35153e748f9feddf86
Glitch (ossified-ahead-zircon) <none>
2020-03-13 12:37:18 +0000

    💡🙅 Checkpoint ./server.js:12883638/350

--- server.js
+++ server.js
@@ -24,8 +24,16 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/", (req,res)=>{
-  res.json({yo: "hello"})
+app.get("/api/timestamp/:date_string?", (req,res)=>{
+  console.log(req.params.date_string);
+  let result;
+  if(req.params.date_string) {
+    
+  } else {
+    result = new Date();
+  }
+  
+  res.json({unix: result})
 })
 
 
